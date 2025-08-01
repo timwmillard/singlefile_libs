7c0593837cb2462b40e5271681c2e298381c2fa4
Tim Millard <timwmillard@gmail.com>
2025-08-01 23:25:21 +1000

    types update

--- snips/types.c
+++ snips/types.c
@@ -7,18 +7,18 @@
 #include <stdbool.h>
 
 // unsigned integer type with width of exactly 8, 16, 32 and 64 bits respectively.
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef __uint128_t u128;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+typedef __uint128_t uint128;
 
 // signed integer type with width of exactly 8, 16, 32 and 64 bits respectively.
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-typedef __int128_t i128;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef __int128_t int128;
 typedef unsigned int uint;
 
 // size is commonly used for array indexing and loop counting.
@@ -30,9 +30,9 @@ typedef uintptr_t uptr;
 typedef intptr_t iptr;
 
 // floating point
-typedef float f32;
-typedef double f64;
-typedef long double f128;
+typedef float float32;
+typedef double float64;
+typedef long double float128;
 
 /* end standard types */
 
